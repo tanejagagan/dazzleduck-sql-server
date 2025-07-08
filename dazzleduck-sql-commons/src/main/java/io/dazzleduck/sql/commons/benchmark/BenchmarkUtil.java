8e54ebd46d62e0828afd6af0a45e0285ba20c408
tanejgagan@yahoo.com <gtaneja@vijayshrees-mbp.lan>
2025-07-08 11:49:00 -0700

    Added main to benchmark util

--- dazzleduck-sql-commons/src/main/java/io/dazzleduck/sql/commons/benchmark/BenchmarkUtil.java
+++ dazzleduck-sql-commons/src/main/java/io/dazzleduck/sql/commons/benchmark/BenchmarkUtil.java
@@ -1,22 +1,51 @@
 package io.dazzleduck.sql.commons.benchmark;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import io.dazzleduck.sql.commons.ConnectionPool;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class BenchmarkUtil {
 
+    public static void main(String []args  ) throws SQLException, IOException {
+        var argv = new Args();
+        JCommander.newBuilder()
+                .addObject(argv)
+                .build()
+                .parse(args);
+        var genFun = argv.benchmarkName.equals("tpcds") ? "dsdgen" : "dbgen";
+        createBenchmarkData(argv.benchmarkName, genFun, argv.scaleFactor, argv.path);
+    }
+
+    public static class Args {
+        @Parameter(names = "--path", description = "Location where files will be written")
+        String path;
+
+        @Parameter(names = {"--sf", "--scale_factor"}, description = "scale factor")
+        String scaleFactor;
+
+        @Parameter(names = {"--name"}, description = "Name of the benchmark [tpcdds, tpc]")
+        String benchmarkName;
+
+    }
+
     public static void createBenchmarkData(String benchmark,
-                                            String genFun,
-                                            String scaleFactor,
-                                            String dataPathStr) throws SQLException {
+                                           String genFun,
+                                           String scaleFactor,
+                                           String dataPathStr) throws SQLException, IOException {
+        Files.createDirectories(Path.of(dataPathStr));
         String[] sqls = {
                 String.format("INSTALL %s", benchmark),
                 String.format("LOAD %s", benchmark),
                 String.format("CALL %s(sf = %s)", genFun, scaleFactor)
         };
         try (Connection connection  = ConnectionPool.getConnection()) {
+            System.out.println("Executing generation");
             ConnectionPool.executeBatch(connection, sqls);
 
             // Copy data to parquet files
