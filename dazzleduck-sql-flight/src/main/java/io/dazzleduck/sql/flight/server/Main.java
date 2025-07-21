0a5c91f247c3b86324ab91a6aae1cbbc85a0efc6
Ekant2008 <ekantyadav2008@gmail.com>
2025-07-21 11:07:21 +0530

    check tsl.1

--- dazzleduck-sql-flight/src/main/java/io/dazzleduck/sql/flight/server/Main.java
+++ dazzleduck-sql-flight/src/main/java/io/dazzleduck/sql/flight/server/Main.java
@@ -26,8 +26,7 @@ public class Main {
         Thread severThread = new Thread(() -> {
             try {
                 flightServer.start();
-                flightServer.getLocation().getUri().getScheme().equals("grpc+tls");
-                System.out.println("S1: Server (Location): Listening on port " + flightServer.getPort());
+                System.out.println("S1: Server (Location): Listening on URI and port:  " + flightServer.getLocation().getUri().toString());
                 flightServer.awaitTermination();
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
