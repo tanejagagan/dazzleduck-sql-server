20cc6a5dd4c18a32b80a7d5843edbcf918c0ef27
PiyushMahore <156774805+PiyushMahore@users.noreply.github.com>
2025-11-08 00:17:16 +0530

    removed cancel response record class

--- dazzleduck-sql-http/src/main/java/io/dazzleduck/sql/http/server/CancelResponse.java
+++
@@ -1,7 +0,0 @@
-package io.dazzleduck.sql.http.server;
-
-import javax.annotation.Nullable;
-
-public record CancelResponse(Long id,
-                             String message,
-                             @Nullable String error) { }
