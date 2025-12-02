ab9c1c0174f300f781cd13ee0e3e7ec1dff2ee03
Ekant2008 <ekantyadav2008@gmail.com>
2025-12-02 04:41:43 +0000

    correct names

--- dazzleduck-sql-logger/src/main/java/io/dazzleduck/sql/logger/ArrowSimpleLogger.java
+++ dazzleduck-sql-logger/src/main/java/io/dazzleduck/sql/logger/ArrowSimpleLogger.java
@@ -46,8 +46,8 @@ public class ArrowSimpleLogger extends LegacyAbstractLogger {
 
     // load config ONCE — names unchanged
     private static final Config config = ConfigFactory.load().getConfig("dazzleduck_micrometer");
-    private static final String CONFIG_APPLICATION_ID = config.getString("id");
-    private static final String CONFIG_APPLICATION_NAME = config.getString("name");
+    private static final String CONFIG_APPLICATION_ID = config.getString("applicationId");
+    private static final String CONFIG_APPLICATION_NAME = config.getString("applicationName");
     private static final String CONFIG_HOST = config.getString("host");
 
     private static final DateTimeFormatter TS_FORMAT =
