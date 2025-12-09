1b71fe446b4d2a6dd87fa22f88bd4ff6fca9365e
tanejgagan@yahoo.com <gtaneja@vijayshrees-mbp.lan>
2025-12-09 12:51:15 -0800

    authorization tests

---
+++ dazzleduck-sql-http/src/test/java/io/dazzleduck/sql/http/server/HttServerAuthorizationTest.java
@@ -0,0 +1,10 @@
+package io.dazzleduck.sql.http.server;
+
+/**
+ * Create authorization server
+ * Create tables. Insert some data
+ * Check if the filters are applied when getStream is called.
+ */
+public class HttServerAuthorizationTest {
+
+}
