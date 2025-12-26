043102904ba2318a0cc8d4fe62a1591399784bbe
Ekant2008 <ekantyadav2008@gmail.com>
2025-12-26 09:33:22 +0530

    fix http_client_timeout_ms

--- dazzleduck-sql-micrometer/src/main/java/io/dazzleduck/sql/micrometer/metrics/MetricsRegistryFactory.java
+++ dazzleduck-sql-micrometer/src/main/java/io/dazzleduck/sql/micrometer/metrics/MetricsRegistryFactory.java
@@ -27,7 +27,7 @@ public final class MetricsRegistryFactory {
                 http.getString("username"),
                 http.getString("password"),
                 http.getString("target_path"),
-                Duration.ofMillis(http.getLong("timeout_ms")),
+                Duration.ofMillis(http.getLong("http_client_timeout_ms")),
                 config.getLong("min_batch_size"),
                 Duration.ofMillis(config.getLong("max_send_interval_ms")),
                 config.getLong("max_in_memory_bytes"),
